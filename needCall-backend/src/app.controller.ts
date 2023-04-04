e8136102da9756f5435ff39c5110d89236ae1698
abdessamedhoudani <abdessamed.houdani@gmail.com>
2023-04-04 21:39:29 +0000

    maj structure of project folders

--- needCall-backend/src/app.controller.ts
+++ needCall-backend/src/app.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
@@ -18,4 +18,5 @@ export class AppController {
   getProfile(@Request() req) {
     return req.user;
   }
+
 }
